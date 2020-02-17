61d13ef6131c5bb6dab0a3982eaff6671d0f6b6c
JamesDHW <JamesHaworthWheatman@gmail.com>
2020-02-17 11:37:12 +0000

    checkboxes not working

--- Numberfit/src/app/subject-select/subject-select.page.ts
+++ Numberfit/src/app/subject-select/subject-select.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute} from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-subject-select',
@@ -8,34 +8,52 @@ import { Router, ActivatedRoute} from '@angular/router';
 })
 export class SubjectSelectPage implements OnInit {
 
+  public subject: string;
+  langs;
+  subForm;
+
   constructor(
     public router: Router,
-    public activatedRoute: ActivatedRoute) { }
+    public activatedRoute: ActivatedRoute) {
+      this.subject = "Addition"
+     }
+
+  radioGroupChange(event){
+    // this.subject = event.target.value;
+    console.log(event.detail)
+  };
 
   ngOnInit() {
     let gamemode = this.activatedRoute.snapshot.paramMap.get("gamemode");
     let subjects = ['Addition', 'Subtraction', 'Multiplication', 'Division'];
     var radioGroup = document.getElementById("radio-group");
     var btnPlay = document.getElementById("btn-play");
+    var windowObject = this;
+
+    // Set the title according to the gamemode
+    if(Number(gamemode) === 0){
+      document.getElementById("title").textContent = "Single Player";
+    } else {
+      document.getElementById("title").textContent = "Head-to-Head";
+    }
 
     // Add subjects to the ion-radio-group
     for (var i = 0; i < subjects.length; i++) {
-      radioGroup.innerHTML += "<ion-item> <ion-label>"+subjects[i]+"</ion-label>" +
-      "<ion-radio slot='end' (click) ='radioSelect("+subjects[i]+")'</ion-radio> </ion-item>";
+      radioGroup.innerHTML += "<ion-item> <ion-label>"+subjects[i]+"</ion-label>"+
+      "<ion-radio slot='end' value='"+subjects[i]+"'</ion-radio></ion-item>";
     }
 
-    function radioSelect(subject){
-      radioGroup.setAttribute('value', subject);
-      console.log("subject");
-    };
+    // Add eventListener for form submission
+    btnPlay.addEventListener("click", function(){
+      let radios = this.getElementsByTagName('ion-radio');
+      // Navigate to the respective page
+      if(Number(gamemode) === 0){
+        windowObject.router.navigate(['/play-single', windowObject.subject]);
+      } else {
+        windowObject.router.navigate(['/play-multi', windowObject.subject]);
+      }
+    });
 
-    if(Number(gamemode) === 0){
-      document.getElementById("title").textContent = "Single Player";
-      btnPlay.addEventListener('click', () => this.router.navigate(['/play-single', radioGroup.getAttribute("value")]));
-    } else {
-      document.getElementById("title").textContent = "Head-to-Head";
-      btnPlay.addEventListener('click', () => this.router.navigate(['/play-multi', radioGroup.getAttribute("value")]));
-    }
   }
 
 }
