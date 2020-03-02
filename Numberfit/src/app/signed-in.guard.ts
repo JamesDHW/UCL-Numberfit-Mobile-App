a90749d8db00fae6ed76afdd282b7c6c2cc39804
JamesDHW <JamesHaworthWheatman@gmail.com>
2020-03-02 12:49:15 +0000

    added a sign-in sign-out guard

---
+++ Numberfit/src/app/signed-in.guard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/auth'
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignedInGuard implements CanActivate {
+
+  constructor(
+      private angularFireAuth: AngularFireAuth,
+      private router: Router,
+    ){ }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.angularFireAuth.authState.pipe(
+        map((auth)=>{
+          if(!auth){
+            this.router.navigate(["/play"]);
+            return false;
+          } else{
+            return true;
+          };
+        }
+      )
+    );
+  }
+
+}
