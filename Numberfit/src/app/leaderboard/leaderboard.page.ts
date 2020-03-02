be06934805d2d07b0eb0370c976d72cb27e80c82
JamesDHW <JamesHaworthWheatman@gmail.com>
2020-03-02 15:40:45 +0000

    Leadeboard page now working (not including the badges)

--- Numberfit/src/app/leaderboard/leaderboard.page.ts
+++ Numberfit/src/app/leaderboard/leaderboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
 
@@ -9,18 +10,53 @@ import { AngularFireAuth } from '@angular/fire/auth';
   styleUrls: ['leaderboard.page.scss'],
 })
 export class HomePage implements OnInit {
+  selectSchoolGroup: FormGroup;
 
   constructor(
     private angularFirestore: AngularFirestore,
     private angularFireAuth: AngularFireAuth,
-  ) {  }
+    formBuilder: FormBuilder,
+  ) {
+    this.selectSchoolGroup = formBuilder.group({
+      school: ["", [Validators.required]],
+    });
+  }
 
   ngOnInit(){
     let schoolSelect = document.getElementById("schoolSelect");
-    console.log(schoolSelect)
+    let myScore = document.getElementById("myScore");
+    let leaderboard = document.getElementById("leaderboard");
+
     this.angularFirestore.collection("Schools").ref.get().then((snapshot)=>{
       snapshot.docs.forEach(doc=>{
         schoolSelect.innerHTML += "<ion-select-option value="+doc.id+">"+doc.id+"</ion-select-option>";
+      })
+    });
+    this.angularFirestore.collection("Users").doc(this.angularFireAuth.auth.currentUser.uid)
+    .ref.get().then((doc)=>{
+      if(doc.exists){
+        let score = doc.data().score;
+        myScore.innerHTML = "My Score: "+score+" points";
+      }
+    });
+    this.angularFirestore.collection("Users").ref.orderBy("score").limit(10)
+    .get().then((snapshot)=>{
+      snapshot.docs.forEach(doc=>{
+        leaderboard.innerHTML += "<ion-item> <ion-label>"+doc.data().name+" scored "
+        +doc.data().score+" points!"+"</ion-label> </ion-item>";
+      })
+    });
+  }
+
+  searchSchool(){
+    let leaderboard = document.getElementById("leaderboard");
+
+    this.angularFirestore.collection("Users").ref
+    .where("school","==",this.selectSchoolGroup.value["school"]).orderBy("score").limit(10)
+    .get().then((snapshot)=>{
+      snapshot.docs.forEach(doc=>{
+        leaderboard.innerHTML += "<ion-item> <ion-label>"+doc.data().name+" scored "
+        +doc.data().score+" points!"+"</ion-label> </ion-item>";
       })
     });
   }
