04d5d9e950df7c757a4265ce1f32fa6197656f07
JamesDHW <JamesHaworthWheatman@gmail.com>
2020-03-02 14:49:40 +0000

    Added stuff into leaderboard card

--- Numberfit/src/app/leaderboard/leaderboard.page.ts
+++ Numberfit/src/app/leaderboard/leaderboard.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: 'leaderboard.page.html',
   styleUrls: ['leaderboard.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(
+    private angularFirestore: AngularFirestore,
+    private angularFireAuth: AngularFireAuth,
+  ) {  }
 
+  ngOnInit(){
+    let schoolSelect = document.getElementById("schoolSelect");
+    console.log(schoolSelect)
+    this.angularFirestore.collection("Schools").ref.get().then((snapshot)=>{
+      snapshot.docs.forEach(doc=>{
+        schoolSelect.innerHTML += "<ion-select-option value="+doc.id+">"+doc.id+"</ion-select-option>";
+      })
+    });
+  }
 }
