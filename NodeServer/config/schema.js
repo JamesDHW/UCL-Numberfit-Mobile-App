ba888f9eaa07f660f154f342c7aa23158516ba4a
Jack Fraser-Nash <jackfn96@gmail.com>
2020-04-20 17:55:27 +0100

    Game, GameHistory schemas added

--- NodeServer/config/schema.js
+++ NodeServer/config/schema.js
@@ -26,9 +26,29 @@ const Teachers = new Schema({
   school   : String,
 });
 
+// Game schema
+const Game = new Schema({
+  topic : String,
+  year  : Int,
+  played: Int,
+  questions   : [],
+  answers    : [],
+});
+
+// *** read the played value to establish the difficulty level of the game
+// val < 9 = beg, 9 < val < 18 = beg, 18 < val < 27 = adv
+const GameHistory = new Schema({
+  played    : Int,
+  correct   : Int,
+  incorrect : Int,
+});
+
+// Declaring each Schema
 const User    = mongoose.model('users', Users, 'users')
 const Pupil   = mongoose.model('pupils', Pupils, 'pupils')
 const Teacher = mongoose.model('teachers', Teachers, 'teachers')
+const Game = mongoose.model('game', Game, 'game')
+
 
 // Include all models required to export here!!!
 module.exports = {
