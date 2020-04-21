0ac6636ae5a67013b57f75682a8b6082b5bada2a
JamesDHW <JamesHaworthWheatman@gmail.com>
2020-04-21 17:12:20 +0100

    changes to deploy

--- NodeServer/config/schema.js
+++ NodeServer/config/schema.js
@@ -27,27 +27,27 @@ const Teachers = new Schema({
 });
 
 // Game schema
-const Game = new Schema({
-  topic : String,
-  year  : Int,
-  played: Int,
-  questions   : [],
-  answers    : [],
+const Games = new Schema({
+  topic     : String,
+  year      : Number,
+  played    : Number,
+  questions : [],
+  answers   : [],
 });
 
 // *** read the played value to establish the difficulty level of the game
 // val < 9 = beg, 9 < val < 18 = beg, 18 < val < 27 = adv
 const GameHistory = new Schema({
-  played    : Int,
-  correct   : Int,
-  incorrect : Int,
+  played    : Number,
+  correct   : Number,
+  incorrect : Number,
 });
 
 // Declaring each Schema
 const User    = mongoose.model('users', Users, 'users')
 const Pupil   = mongoose.model('pupils', Pupils, 'pupils')
 const Teacher = mongoose.model('teachers', Teachers, 'teachers')
-const Game = mongoose.model('game', Game, 'game')
+const Game = mongoose.model('game', Games, 'game')
 
 
 // Include all models required to export here!!!
